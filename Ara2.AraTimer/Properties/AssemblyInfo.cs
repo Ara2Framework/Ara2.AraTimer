5ed1b460be51f815268f1d3e4117013a53664f2d
HenriqueVictorino <henriquevictorino@gmail.com>
2016-04-15 16:12:49 -0300

    Organization review

--- Ara2.AraTimer/Properties/AssemblyInfo.cs
+++ Ara2.AraTimer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Ara2.AraTimer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Ara timer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ara2FrameWork")]
 [assembly: AssemblyProduct("Ara2.AraTimer")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright (c) 2010-2016, Rafael Leonel Pontani. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
